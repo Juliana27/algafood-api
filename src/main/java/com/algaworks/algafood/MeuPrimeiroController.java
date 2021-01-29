88f1294db1253ee6cac4524a89621a6aa516cabe
jagb <jagb@gft.com>
2021-01-29 10:47:49 -0300

    curso de api rest primeiro commit

---
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
+@Controller
+public class MeuPrimeiroController {
+
+	private AtivacaoClienteService ativacaoClienteService;
+	
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+		System.out.println("Meu primeiro controller " + ativacaoClienteService);
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		Cliente joao = new Cliente("João", "joao@gmail.com", "34978764543");
+		ativacaoClienteService.ativar(joao);
+		return "Hello!";
+	}
+}
